c129158a1041d2b37cd71024403906cf2744c52e
FahadNisar7223 <nisarfahad04@gmail.com>
2022-11-07 20:43:05 +0500

    container class created

--- BMI_Calculator/lib/main.dart
+++ BMI_Calculator/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'inputpage.dart';
+
+
 void main(){
-  runApp(inputpage());
+  runApp(BMIcalculator());
 }
 
-class inputpage extends StatelessWidget {
-  const inputpage({Key? key}) : super(key: key);
+class BMIcalculator extends StatelessWidget {
+  const BMIcalculator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: input(),
+      home: inputpage(),
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
@@ -17,22 +20,3 @@ class inputpage extends StatelessWidget {
     );
   }
 }
-
-class input extends StatefulWidget {
-  const input({Key? key}) : super(key: key);
-
-  @override
-  State<input> createState() => _inputState();
-}
-
-class _inputState extends State<input> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:Text("BMI Calculator"),),
-    );
-  }
-}
-
-
-
