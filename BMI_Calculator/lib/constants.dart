be0f39177454499e2a54a81fc9065cb5f8e4e77c
FahadNisar7223 <nisarfahad04@gmail.com>
2022-11-07 21:30:46 +0500

    class completed for constants

---
+++ BMI_Calculator/lib/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const Klabelstyle=TextStyle(
+  fontSize: 20.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 40.0,
+  fontWeight: FontWeight.bold,
+);
+
+const activecolor = Color(0xFF1D1E33);
+const deactivecolor = Color(0xFF111328);
